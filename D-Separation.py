a471823e4382bbf70c20054bb97935251a1a3818
Akhil Chandran <33461269+AkhilTrc@users.noreply.github.com>
2021-12-08 22:35:25 +0100

    Additionals to dsep in DSeparation class.

--- D-Separation.py
+++ D-Separation.py
@@ -1,28 +1,45 @@
 from BNReasoner import BNReasoner
+from BayesNet import BayesNet
 from collections import deque
+from typing import Union, set
 import networkx as nx
 
 
 class DSeparation(BNReasoner):
+    def __init__(self, net: Union[str, BayesNet], X: set, Y: set, Z: set):
+        super().__init__(net)
+        self.X, self.Y, self.Z = X, Y, Z
+
     def execute(self):
-        pass
+        result = self.dsep(X, Y, Z)
+        return result
 
     def dsep(self, X, Y, Z):
-        g = self.bn.copy()
         XYZ = X.union(Y).union(Z)
 
-        # Find and remove leaf nodes that are not part of the x, y, z union.
+        # Pruning Step 1: Find and remove leaf nodes that are not part of the X, Y, Z union.
         #
         l_nodes = deque()
-        for node in g.nodes:
-            if g.out_degree[node] == 0:     # Finds all the leaf nodes.
+        for node in self.nodes:
+            if len(self.bn.get_children(node)) == 0:
                 l_nodes.append(node)
         while len(l_nodes) > 0:
             l = l_nodes.popleft()
             if l not in XYZ:
-                # need a way to add parent to l.
-                g.bn.del_var(l)
-
-        g.remove_edges_from(g.out_edge(Z))  # Removes all edges outgoing from nodes in Z.
+                for p in self.predecessors(l):
+                    if self.out_degree[p] == 1:
+                        l_nodes.append(p)
+                self.bn.del_var(l)
+        
+        # Pruning Step 2: Removes all edges outgoing from nodes in Z.
+        #
+        self.remove_edges_from(list(self.out_edge(Z)))  
 
         # To check closedness via Z.
+        #
+        
+
+X, Y, Z = {"bowel-problem"}, {"light-on"}, {"hear-bark"}
+bnReasoner = DSeparation('testing/dog_problem.BIFXML', X, Y, Z)
+bnReasoner.execute()
+
