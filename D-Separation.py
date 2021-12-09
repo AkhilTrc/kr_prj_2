c84d80b2dfd17aae6f1224eff982ffe0fcffbd9f
Akhil Chandran <33461269+AkhilTrc@users.noreply.github.com>
2021-12-09 10:06:26 +0100

    Modified BaysNet and D-Separation.

--- D-Separation.py
+++ D-Separation.py
@@ -1,8 +1,9 @@
 from BNReasoner import BNReasoner
 from BayesNet import BayesNet
 from collections import deque
-from typing import Union, set
+from typing import Union ################################## set
 import networkx as nx
+from networkx.utils import UnionFind
 
 
 class DSeparation(BNReasoner):
@@ -20,26 +21,46 @@ class DSeparation(BNReasoner):
         # Pruning Step 1: Find and remove leaf nodes that are not part of the X, Y, Z union.
         #
         l_nodes = deque()
-        for node in self.nodes:
+        for node in set(self.bn.get_all_variables()):
             if len(self.bn.get_children(node)) == 0:
                 l_nodes.append(node)
         while len(l_nodes) > 0:
             l = l_nodes.popleft()
             if l not in XYZ:
-                for p in self.predecessors(l):
-                    if self.out_degree[p] == 1:
+                for p in self.bn.get_parents(l):
+                    if len(self.bn.get_children(p)) == 1:
                         l_nodes.append(p)
                 self.bn.del_var(l)
         
         # Pruning Step 2: Removes all edges outgoing from nodes in Z.
         #
-        self.remove_edges_from(list(self.out_edge(Z)))  
+        # for edge in list(self.bn.structure.out_edge(Z)):
+            # self.bn.del_edge(edge[0], edge[1])
+            # self.bn.del_edges_from(list(self.out_edge(Z)))  
 
-        # To check closedness via Z.
+        self.bn.del_edges_from(self.bn.get_out_edge(Z))
+
+        # To find connected paths in the pruned graph G'.
+        # Ignoring Directedness i.e. udg -> Undirected Graph represented using disjoint sets.
+        # Get weakly connected components.
+        # 
+        udg = UnionFind(set(self.bn.get_all_variables()))
+        g = self.bn.copy_graph()
+        for w_node in self.bn.wcc():        # Error here.
+            udg.union(*w_node) 
+        # Merge all. 
+        udg.union(*X)
+        udg.union(*Y)
+
+        # False if atleast one connection found.
         #
+        if X and Y and udg[next(iter(X))] == udg[next(iter(Y))]:
+            return False
+        else:
+            return True 
         
-
-X, Y, Z = {"bowel-problem"}, {"light-on"}, {"hear-bark"}
+        
+X, Y, Z = {"light-on"}, {"dog-out"}, {"family-out"}
 bnReasoner = DSeparation('testing/dog_problem.BIFXML', X, Y, Z)
-bnReasoner.execute()
+print (bnReasoner.execute())
 
